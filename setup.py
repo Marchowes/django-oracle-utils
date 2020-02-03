d7ae7cb2293f1f7fcdaf58bebc1f15c46ca7c2a0
Robbie Meyers <robbie.meyers@gmail.com>
2020-02-03 13:53:57 -0500

    update name (#3)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="django-oracle-utils-rmeyers4", # Replace with your own username
+    name="django-oracle-utils", # Replace with your own username
     version="0.0.1",
     author="Robbie Meyers",
     author_email="robbie.meyers@gmail.com",
